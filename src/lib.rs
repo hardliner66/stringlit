e7a44c79606b61338fa4f70352c5c0772b005c69
Steve Biedermann <steve.biedermann@mquadr.at>
2019-04-25 12:54:35 +0200

    (c) force result of macro to be a string (+) test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
 #[macro_export]
 macro_rules! s {
-    ($e:literal) => {$e.to_owned()};
+    ($e:literal) => {$e.to_owned() as String};
 }
 
 #[macro_export]
 macro_rules! string {
-    ($e:literal) => {$e.to_owned()};
+    ($e:literal) => {$e.to_owned() as String};
+}
+
+#[cfg(test)]
+mod test {
+    // enforces same type
+    fn strict_equals<T: std::cmp::PartialEq>(a: T, b: T) -> bool {
+        return a == b;
+    }
+
+    #[test]
+    fn same_type_and_value() {
+        assert!(strict_equals(s!("abcd"), "abcd".to_owned()));
+        assert!(strict_equals(string!("abcd"), "abcd".to_owned()));
+        println!("OK");
+    }
 }
